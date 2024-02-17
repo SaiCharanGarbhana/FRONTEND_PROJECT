33d2745504985fcc8778650990bf18d366632787
saicharan <saicharangarbhana1809@gmail.com>
2024-02-17 13:00:58 +0530

    my first commit

---
+++ src/test_1.js
@@ -0,0 +1,48 @@
+for(let i = 0;i<10;i++){
+    console.log(i  + "the i value ");
+ }
+
+
+ let h = 10;
+ while(h<50){
+    h++;
+    console.log(h)
+ }
+
+
+let Day = "sunday";
+switch(Day){
+    case "sunday" :{
+        console.log("yehhhh its  sunday");
+        break;
+    }
+    case "monday" : {
+        console.log("its monday");
+        break;
+    }
+    case "tuesday" :{
+        console.log("its tuesday");
+        break;
+    }
+    case "webnesday" :{
+        console.log("its wednesday");
+        break;
+    }
+    case "thursday" :{
+        console.log("its thursday");
+        break;
+    }
+    case "friday" :{
+        console.log("its friday");
+        break;
+    }
+    case "saturday" :{
+        console.log("its saturday");
+        break;
+    }
+default :{
+    console.log("this is default");
+    break;
+}
+
+}
